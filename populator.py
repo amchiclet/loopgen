60959b0df0ea6e48e4ca66c103c876c90808c23f
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2021-03-07 12:41:27 -0800

    Moved populating functions to populator module

--- populator.py
+++ populator.py
@@ -1,5 +1,5 @@
 import random
-from skeleton_ast import Replacer, Const, Declaration, Node
+from skeleton_ast import Replacer, Const, Declaration, Node, replace, ExpressionHole, StatementHole, OpHole, NameHole
 
 class IncrementalChoice:
     def __init__(self):
@@ -10,7 +10,17 @@ class IncrementalChoice:
         self.index += 1
         return sample
 
-class Populator(Replacer):
+class FixedChoice:
+    def __init__(self, choices):
+        self.choices = choices
+        self.index = 0
+    def choice(self, population):
+        assert(self.i < len(self.choices))
+        sample = population[self.choices[self.index]]
+        self.index += 1
+        return sample
+
+class PopulateParameters:
     def __init__(self, default_choices=None, is_finite=False, choice_function=random.choice):
         self.available = {}
         self.choice_functions = {}
@@ -45,3 +55,45 @@ class Populator(Replacer):
             self.available[family].remove(chosen)
 
         return chosen.clone()
+
+def populate_name(program, populate_function):
+    replacer = NamePopulator(populate_function)
+    return replace(program, replacer)
+
+def populate_stmt(program, populate_function):
+    replacer = StatementPopulator(populate_function)
+    return replace(program, replacer)
+
+def populate_expr(program, populate_function):
+    replacer = ExpressionPopulator(populate_function)
+    return replace(program, replacer)
+
+def populate_op(program, populate_function):
+    replacer = OpPopulator(populate_function)
+    return replace(program, replacer)
+
+class Populator(Replacer):
+    def __init__(self, populate_function):
+        self.populate_function = populate_function
+    def should_skip(self, node):
+        return type(node) in [Declaration, Const]
+    def should_replace(self, node):
+        raise NotImplementedError('Populator::should_replace')
+    def replace(self, name):
+        return self.populate_function(name)
+
+class StatementPopulator(Populator):
+    def should_replace(self, node):
+        return type(node) == StatementHole
+
+class OpPopulator(Populator):
+    def should_replace(self, node):
+        return type(node) == OpHole
+
+class ExpressionPopulator(Populator):
+    def should_replace(self, node):
+        return type(node) == ExpressionHole
+
+class NamePopulator(Populator):
+    def should_replace(self, node):
+        return type(node) == NameHole
